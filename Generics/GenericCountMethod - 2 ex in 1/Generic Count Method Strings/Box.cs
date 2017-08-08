fe2169b13bed68ad250ce9d58e69ecc8a1aebff2
mitkoostz <todorinio123@gmail.com>
2017-08-08 14:59:13 +0300

    Added 2 exercies in one Generic Count Method (with DOUBLE and STRINGS)

---
+++ Generics/GenericCountMethod - 2 ex in 1/Generic Count Method Strings/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Count_Method_Strings
+{
+    public static class Box 
+    {
+
+        public static int Compare<T>(List<T> list , T element)
+            where T : IComparable
+        {
+            int counter = 0;
+
+            foreach (var item in list)
+            {
+                if(item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+
+        }
+    }
+}
---
+++ Generics/GenericCountMethod - 2 ex in 1/Generic Count Method Strings/startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Count_Method_Strings
+{
+    public class startup
+    {
+        public static void Main()
+        {
+            List<double> list = new List<double>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var text = double.Parse(Console.ReadLine());
+                list.Add(text);
+            }
+
+            var lastInput = double.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine(Box.Compare(list, lastInput));
+        }
+    }
+}
